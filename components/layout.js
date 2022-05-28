5e3860fe63ebbae95f002d2a784fba49d2b24e84
behnam1369 <behnam.aghaali@yahoo.com>
2022-05-28 19:15:01 +0430

    Use css modules

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import * as Style from './layout.module.css'
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className={Style.container}>
       {children}
-    </>
+    </div>
   )
 }
