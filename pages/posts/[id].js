fe3a5a46bb46da06440456f3785edfc956ca3ce4
behnam1369 <behnam.aghaali@yahoo.com>
2022-05-29 13:32:57 +0430

    Set header for post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,12 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
 
 export default function post({ postData }) {
   return <Layout>
+    <Head>
+      <title>{postData.title}</title>
+    </Head>
     <h1>{postData.title}</h1>
     <h2>{postData.id}</h2>
     <span>{postData.date}</span>
