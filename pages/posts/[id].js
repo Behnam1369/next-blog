d8911c8149caac9bfb8b504e48be1fd8762fcc4a
behnam1369 <behnam.aghaali@yahoo.com>
2022-05-29 13:06:07 +0430

    Render Markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,7 +5,8 @@ export default function post({ postData }) {
   return <Layout>
     <h1>{postData.title}</h1>
     <h2>{postData.id}</h2>
-    <p>{postData.date}</p>
+    <span>{postData.date}</span>
+    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
   </Layout>
 }
 
@@ -18,7 +19,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
