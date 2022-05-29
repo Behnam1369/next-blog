c276cccd6ccac4cb756f4ccb7173eb843017bb23
behnam1369 <behnam.aghaali@yahoo.com>
2022-05-29 13:41:45 +0430

    Add styles

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,17 +2,23 @@ import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
 import Date from '../../components/date';
+import Styles from '../../style/utils.module.css'
 
 export default function post({ postData }) {
-  return <Layout>
-    <Head>
-      <title>{postData.title}</title>
-    </Head>
-    <h1>{postData.title}</h1>
-    <h2>{postData.id}</h2>
-    <Date dateString={postData.date} />
-    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-  </Layout>
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={Styles.headingXl}>{postData.title}</h1>
+        <div className={Styles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
